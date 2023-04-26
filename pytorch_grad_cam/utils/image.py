ec4653adae463dca475e4318623d5206814f46f8
trex22 <contact@jasonchalom.com>
2023-04-26 22:54:05 +0200

    Switch dimensions :cop:

--- pytorch_grad_cam/utils/image.py
+++ pytorch_grad_cam/utils/image.py
@@ -166,7 +166,7 @@ def scale_cam_image(cam, target_size=None, compute_device=None):
         compute_device = torch.device("cpu")
 
     if target_size is not None:
-        result = torch.zeros([cam.shape[0], target_size[1], target_size[0]]) # TODO: Swap back and remove transpose
+        result = torch.zeros([cam.shape[0], target_size[0], target_size[1]]) # TODO: Swap back and remove transpose
     else:
         result = torch.zeros(cam.shape)
 
